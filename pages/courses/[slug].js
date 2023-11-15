3e3f93de9b28703408a1c4d6ce2f59f240b672cc
Devesh <deveshamway@gmail.com>
2023-11-15 16:00:54 +0530

    get course data on course page

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -5,19 +5,49 @@ import {
   Keypoints
 } from "@components/course";
 import { BaseLayout } from "@components/layout";
+import { getAllCourses } from "@content/courses/fetcher";
 
-export default function Course() {
+export default function Course({course}) {
 
   return (
     <>
       <div className="py-4">
-        <CourseHero />
+        <CourseHero
+          title={course.title}
+          description={course.description}
+          image={course.coverImage}
+        />
       </div>
       <Keypoints />
       <Curriculum />
       <Modal />
     </>
   )
+}
+
+export function getStaticPaths() {
+  const { data } = getAllCourses()
+
+  return {
+    paths: data.map(c => ({
+      params: {
+        slug: c.slug
+      }
+    })),
+    fallback: false
+  }
+}
+
+
+export function getStaticProps({params}) {
+  const { data } = getAllCourses()
+  const course = data.filter(c => c.slug === params.slug)[0]
+
+  return {
+    props: {
+      course
+    }
+  }
 }
 
 Course.Layout = BaseLayout
