392f35b378fe4e46707eff12696191e25f4cdbab
Devesh <deveshamway@gmail.com>
2023-11-16 14:45:53 +0530

    usehooks abstraction

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -4,6 +4,7 @@ import detectEthereumProvider from "@metamask/detect-provider";
 import Web3 from "web3";
 import { setupHooks } from "./hooks/setupHooks";
 
+
 const Web3Context = createContext(null)
 
 export default function Web3Provider({children}) {
@@ -40,7 +41,7 @@ export default function Web3Provider({children}) {
     return {
       ...web3Api,
       isWeb3Loaded: web3 != null,
-      hooks: setupHooks(web3),
+      getHooks: () => setupHooks(web3),
       connect: provider ?
         async () => {
           try {
@@ -62,4 +63,9 @@ export default function Web3Provider({children}) {
 
 export function useWeb3() {
   return useContext(Web3Context)
+}
+
+export function useHooks(cb) {
+  const { getHooks } = useWeb3()
+  return cb(getHooks())
 }
