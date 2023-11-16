73886b4b5c40e58302b0c9331e15f736e99c6475
Devesh <deveshamway@gmail.com>
2023-11-16 19:55:24 +0530

    market pages add

--- components/ui/marketplace/header/index.js
+++ components/ui/marketplace/header/index.js
@@ -1,5 +1,18 @@
+
+
 import { Breadcrumbs } from "@components/ui/common";
 import { EthRates, WalletBar } from "@components/ui/web3";
+
+const LINKS = [{
+  href: "/marketplace",
+  value: "Buy"
+}, {
+  href: "/marketplace/courses/owned",
+  value: "My Courses"
+}, {
+  href: "/marketplace/courses/manage",
+  value: "Manage Courses"
+}]
 
 export default function Header() {
   return (
@@ -7,7 +20,7 @@ export default function Header() {
       <WalletBar />
       <EthRates />
       <div className="flex flex-row-reverse pb-4 px-4 sm:px-6 lg:px-8">
-        <Breadcrumbs />
+        <Breadcrumbs items={LINKS} />
       </div>
     </>
   )
