4da956fe057d515f425fdfa5a4ddfe9400fe6b12
Devesh <deveshamway@gmail.com>
2023-11-16 18:37:56 +0530

    added button in course card

--- components/ui/web3/walletbar/index.js
+++ components/ui/web3/walletbar/index.js
@@ -1,10 +1,10 @@
 import { useWeb3 } from "@components/providers"
 
 
-export default function WalletBar({address, network}) {
+export default function WalletBar({ address, network }) {
   const { requireInstall } = useWeb3()
   return (
-    <section className="text-white bg-indigo-600">
+    <section className="text-white bg-indigo-600 rounded-lg">
       <div className="p-8">
         <h1 className="text-2xl">Hello, {address}</h1>
         <h2 className="subtitle mb-5 text-xl">I hope you are having a great day!</h2>
@@ -17,7 +17,7 @@ export default function WalletBar({address, network}) {
             </div>
           </div>
           <div>
-            { network.hasInitialResponse && !network.isSupported &&
+            {network.hasInitialResponse && !network.isSupported &&
               <div className="bg-red-400 p-4 rounded-lg">
                 <div>Connected to wrong network</div>
                 <div>
@@ -28,12 +28,12 @@ export default function WalletBar({address, network}) {
                 </div>
               </div>
             }
-            { requireInstall &&
+            {requireInstall &&
               <div className="bg-yellow-500 p-4 rounded-lg">
                 Cannot connect to network. Please install Metamask.
               </div>
             }
-            { network.data &&
+            {network.data &&
               <div>
                 <span>Currently on </span>
                 <strong className="text-2xl">{network.data}</strong>
