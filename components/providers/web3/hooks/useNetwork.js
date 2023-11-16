740c6ecd54ebaae67bdf0060d30498d08bfbfe7b
Devesh <deveshamway@gmail.com>
2023-11-16 16:29:16 +0530

    target network

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -2,32 +2,37 @@ import { useEffect } from "react"
 import useSWR from "swr"
 
 const NETWORKS = {
-    1: "Ethereum Main Network",
-    59144: "Linea Main Network",
-    1337: "Ganache",
-    137: "Polygon Main Network",
-  }
+	1: "Ethereum Main Network",
+  59144: "Linea Main Network",
+  1337: "Ganache",
+  137: "Polygon Main Network",
+}
+
+const targetNetwork = NETWORKS[process.env.NEXT_PUBLIC_TARGET_CHAIN_ID]
 
 export const handler = (web3, provider) => () => {
 
-  const { mutate, ...rest } = useSWR(() =>
+  const { data, mutate, ...rest } = useSWR(() =>
     web3 ? "web3/network" : null,
     async () => {
-        const chainId = await web3.eth.getChainId()
-        return NETWORKS[chainId]
+      const chainId = await web3.eth.getChainId()
+      return NETWORKS[chainId]
     }
   )
 
   useEffect(() => {
     provider &&
     provider.on("chainChanged", chainId => {
-        mutate(NETWORKS[parseInt(chainId, 16)])
-      })
+      mutate(NETWORKS[parseInt(chainId, 16)])
+    })
   }, [web3])
 
   return {
     network: {
+      data,
       mutate,
+      target: targetNetwork,
+      isSupported: data === targetNetwork,
       ...rest
     }
   }
