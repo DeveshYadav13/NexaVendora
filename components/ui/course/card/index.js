3662ecf5374ccf1ab0b89d17e6f9b91e5735992b
Devesh <deveshamway@gmail.com>
2023-11-17 15:08:10 +0530

    walletbar style improvements

--- components/ui/course/card/index.js
+++ components/ui/course/card/index.js
@@ -3,7 +3,7 @@
 import Image from "next/legacy/image"
 import Link from "next/link"
 
-export default function Card({course, disabled, Footer}) {
+export default function Card({ course, disabled, Footer }) {
   return (
     <div
       className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
@@ -25,15 +25,15 @@ export default function Card({course, disabled, Footer}) {
           </div>
           <Link href={`/courses/${course.slug}`} legacyBehavior>
             <a
-              className="h-12 block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+              className="h-12 block mt-1 text-sm sm:text-lg leading-tight font-medium text-black hover:underline">
               {course.title}
             </a>
           </Link>
           <p
-            className="mt-2 text-gray-500">
+            className="mt-2 text-sm sm:text-base text-gray-500">
             {course.description.substring(0, 70)}...
           </p>
-          { Footer &&
+          {Footer &&
             <Footer />
           }
         </div>
